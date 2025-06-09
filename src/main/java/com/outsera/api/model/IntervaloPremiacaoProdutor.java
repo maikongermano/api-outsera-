679f78f2b855cf1a7f080790376b93b90c062d93
Maikon Germano <mk_germano@hotmail.com>
2025-06-09 14:38:40 -0300

    fix: correção retorno chamada

--- src/main/java/com/outsera/api/model/IntervaloPremiacaoProdutor.java
+++ src/main/java/com/outsera/api/model/IntervaloPremiacaoProdutor.java
@@ -1,14 +1,28 @@
 package com.outsera.api.model;
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class IntervaloPremiacaoProdutor {
+	
+	@JsonProperty("producer")
     private String produtor;
+	
+	@JsonProperty("interval")
     private int intervalo;
+	
+	@JsonProperty("previousWin")
     private int anoAnterior;
+	
+	@JsonProperty("followingWin")
     private int anoSeguinte;
+
 }
--- src/main/java/com/outsera/api/model/ResultadoIntervaloPremiacao.java
+++ src/main/java/com/outsera/api/model/ResultadoIntervaloPremiacao.java
@@ -1,14 +1,24 @@
 package com.outsera.api.model;
 
-import lombok.*;
-
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class ResultadoIntervaloPremiacao {
-    private List<IntervaloPremiacaoProdutor> intervalosMinimos;
-    private List<IntervaloPremiacaoProdutor> intervalosMaximos;
+	
+	@JsonProperty("min")
+	private List<IntervaloPremiacaoProdutor> intervalosMinimos;
+	
+	@JsonProperty("max")
+	private List<IntervaloPremiacaoProdutor> intervalosMaximos;
+
 }
